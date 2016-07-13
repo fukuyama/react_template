1bbbf08af65d54a7a433e1ff96f78bb1d2ef9a8c
Yoshihito Fukuyama <yoshihito.fukuyama@gmail.com>
2016-07-13 21:55:00 +0900

    add event id

--- src/main/jsx/Event.jsx
+++ src/main/jsx/Event.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
+function pad (n, len) {
+  var s = n.toString();
+  while (s.length < len) {
+    s = '0' + s;
+  }
+  return s;
+};
+
 export default function Event({ event }) {
   return (
     <div className="eventNode">
-      name : {event.name}
+      <table>
+        <tr>
+        <td>{pad(event.id,3)}</td>
+        <td>{event.name}</td>
+        </tr>
+      </table>
     </div>
   );
 }
